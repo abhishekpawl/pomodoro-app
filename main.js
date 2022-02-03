cbac888cfdb7b51ef6356e7fb3fd6ba41f9ab838
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-02-04 02:24:35 +0530

    Added startTimer() and getRemainingTime() to start the timer

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
+let interval;
+
 const timer = {
     pomodoro: 25,
     shortBreak: 5,
     longBreak: 15,
     longBreakInterval: 4
 };
+
+const getRemainingTime = (endTime) => {
+    const currentTime = Date.parse(new Date());
+    const difference = endTime - currentTime;
+
+    const total = Number.parseInt(difference / 1000, 10);
+    const minutes = Number.parseInt((total / 60) % 60, 10);
+    const seconds = Number.parseInt(total % 60, 10);
+
+    return {
+        total,
+        minutes,
+        seconds
+    };
+}
+
+const startTimer = () => {
+    let { total } = timer.remainingTime;
+    const endTime = Date.parse(new Date()) + total * 1000;
+
+    interval = setInterval(() => {
+        timer.remainingTime = getRemainingTime(endTime);
+        updateClock();
+
+        total = timer.remainingTime.total;
+        if (total <= 0) {
+            clearInterval(interval);
+        }
+    }, 1000);
+}
 
 const updateClock = () => {
     const { remainingTime } = timer;
@@ -43,3 +75,12 @@ const longBreakButton = document.querySelector('#js-long-break');
 pomodoroButton.addEventListener('click', clickHandler);
 shortBreakButton.addEventListener('click', clickHandler);
 longBreakButton.addEventListener('click', clickHandler);
+
+const mainButton = document.querySelector('#js-btn');
+
+mainButton.addEventListener('click', () => {
+    const { action } = mainButton.dataset;
+    if (action === 'start') {
+        startTimer();
+    }
+});
