a6231d23ade0b1d864a98ab0a8b44ee2acec38bc
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-02-04 02:15:01 +0530

    Changing time according to mode

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ const timer = {
     longBreakInterval: 4
 };
 
+const updateClock = () => {
+    const { remainingTime } = timer;
+    const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+    const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+
+    const min = document.getElementById('js-minutes');
+    const sec = document.getElementById('js-seconds');
+
+    min.textContent = minutes;
+    sec.textContent = seconds;
+}
+
 const clickHandler = (e) => {
     console.log(e.target.dataset.mode);
     const { mode } = e.target.dataset;
